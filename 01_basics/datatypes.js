b95bc75e5a29b2a36d0d774bc5e6d888dd60b632
Vivek Agrawal <140902928+111vivek@users.noreply.github.com>
2024-06-08 07:40:00 +0000

    variables

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as a newer version
+//alert(3+3) // we are using node js, not browser
+// code redeability should be high
+let number=5;
+let string=" Vivek"
+let isIt= true
+let State;
+
+//number  // 2 to power 53
+//bigint
+// boolean // true/false
+// String  // " "
+// null   // standalone value
+// undefined
+// symbol=> unique
+
+// object
+
+console.log(typeof null)  // object
+console.log(typeof undefined) // undefined
+
+// typeof null is object
+// type of undefined is undefined
---
+++ 01_basics/variables.js
@@ -0,0 +1,20 @@
+const accountId= 144533;
+let accountEmail= "vivek@gmail.com"
+var accountPassword= "12345"
+accountCity="Jaipur"
+let accountState;
+
+// accountId= 13544 // not allowed
+/*
+var and let are same things
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+accountEmail= "agrawal@gmail.com"
+accountPassword= "789";
+accountCity="Aligarh"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState
+
+])
