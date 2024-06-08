4f2224e9818cf0ce265d83964ddf3bc55bf00d48
Vivek Agrawal <140902928+111vivek@users.noreply.github.com>
2024-06-08 13:39:01 +0000

    01_array

---
+++ 01_basics/array.js
@@ -0,0 +1,30 @@
+const arr1= [1,2,3,4,5,6]
+console.log(arr1[1])
+
+const myArray= new Array(1,2,3,4,5)
+console.log(myArray[2])
+
+const arr= [1,2,3,4,5]
+console.log(arr)
+console.log(typeof arr)
+const newarr= arr.join();
+console.log(newarr)
+console.log(typeof newarr)
+
+//myarr.push
+//myarr.pop
+//myarr.unshift
+//myarr.shift
+
+
+// slice and splice
+
+const myArr=[1,2,3,4,5,6]
+const mya= myArr.slice(1,3)
+console.log(mya)
+console.log(myArr)
+
+const myb= myArr.splice(1,3)
+console.log(myb)
+console.log(myArr)
+
