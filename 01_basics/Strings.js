7431ee54dbbe3a3c59107886c15044869199f736
Vivek Agrawal <140902928+111vivek@users.noreply.github.com>
2024-06-08 11:22:35 +0000

    strings

---
+++ 01_basics/Strings.js
@@ -0,0 +1,19 @@
+const name= "Vivek"
+const age= 20
+console.log(name+ age+ "Value")
+
+console.log(`my name is ${name} and age is ${age}`)
+const gamename= new String('Vivek')
+console.log(gamename)
+console.log(typeof(gamename))
+console.log(gamename[0])
+console.log(gamename.toUpperCase())
+console.log(gamename.charAt(2))
+console.log(gamename.indexOf('e'))
+
+//replace
+//split
+//includes
+//trim
+//slice
+//substring
