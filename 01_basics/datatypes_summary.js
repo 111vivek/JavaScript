95874b88ea958df06aaf7425a454751f71d906e2
Vivek Agrawal <140902928+111vivek@users.noreply.github.com>
2024-06-08 10:34:48 +0000

    datatypes_summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,13 @@
+// Primary: string, Number, boolean, null undefined, symbol, bigInt
+
+// Reference(Non-Primitive): Array, Objects, Functions
+const heroes=["shaktiman", "naagraj", " doga"]
+let myObject= {
+    name: "Vivek",
+    age: 20,
+}
+
+let myFunction= function(){
+    console.log("Vivek")
+}
+myFunction();
