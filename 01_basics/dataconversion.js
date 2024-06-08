50756958a3ad278c0eb9664b39ccd22afd4a3b90
Vivek Agrawal <140902928+111vivek@users.noreply.github.com>
2024-06-08 07:40:39 +0000

    conversion of datatypes

---
+++ 01_basics/dataconversion.js
@@ -0,0 +1,19 @@
+let score ="33";
+console.log(typeof(score))
+let valueNumber= Number(score)
+console.log(typeof(valueNumber))
+let score1=""
+console.log(typeof(score1));
+let x;
+console.log(typeof(x));
+
+
+// "33"=> 33
+// NaN=> Number
+//"33abc"=> NaN, return Number
+// boolean: true=> 1, false=> 0;
+
+//boolean
+// 1=> true ; 0=> false
+// ""=> false
+//"Vivek"=> true
