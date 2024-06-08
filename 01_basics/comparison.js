0c47d2586d407956b93268e2ae67991800d14ee7
Vivek Agrawal <140902928+111vivek@users.noreply.github.com>
2024-06-08 11:28:57 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,9 @@
+console.log("2">1)
+console.log("2">=1)
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+// === checks the datatypes as well
+console.log("2"===2);
+console.log("2"==2);
