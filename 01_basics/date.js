7206cadc131050bdc7b67cc6dfaeeab9204d2ae5
Vivek Agrawal <140902928+111vivek@users.noreply.github.com>
2024-06-08 12:51:03 +0000

    numsandmaths

---
+++ 01_basics/date.js
@@ -0,0 +1,7 @@
+// const mydate= new Date()
+// console.log(mydate)
+
+// console.log(mydate.toString())
+// console.log(typeof(mydate))
+let mycreatedDate= new Date(2023,0,23)
+console.log(mycreatedDate.toDateString())
---
+++ 01_basics/nums_and_maths.js
@@ -0,0 +1,18 @@
+// const score= 400
+// console.log(score)
+
+// const balance= new Number(500)
+// console.log(balance)
+// console.log(balance.toString())
+//toFixed => ... eg: 400.00
+
+const number= 37.898
+console.log(number.toPrecision(3))
+//**************************maths******************** */
+
+console.log(Math)
+console.log(Math.abs(-4)) // used to change the sign from -ve to +ve
+
+const min =10
+const max= 20
+console.log(Math.floor(Math.random()*(max-min+1)+min))
